1a5736843c1223afc682afb0153022f30d9fcc59
Diego Speroni <diegosperoni1@gmail.com>
2018-07-26 14:56:12 -0300

    Using ng-switch and class

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,15 +9,15 @@ import { Component } from '@angular/core';
 export class AppComponent {
   title = 'Platzi-Squear';
   lugares:any = [
-    {active: true, nombre: "floreria la flor"},
-    {active: true, nombre: "Jugueteria el juguete"},
-    {active: false, nombre: "Zapateria el zapato"},
-    {active: true, nombre: "Gomeria la goma"},
-    {active: true, nombre: "floreria la flor"},
-    {active: true, nombre: "Jugueteria el juguete"},
-    {active: true, nombre: "Zapateria el zapato"},
-    {active: true, nombre: "Gomeria la goma"}
+    {cercania: 1, distancia: 1, active: true, nombre:"Floreria la flor"},
+    {cercania: 2, distancia: 5, active: true, nombre:"Jugueteria el juguete"},
+    {cercania: 2, distancia: 4, active: true, nombre:"Gomeria la goma"},
+    {cercania: 1, distancia: 2, active: true, nombre:"Relojeria el reloj"},
+    {cercania: 3, distancia: 10, active: true, nombre:"Chapa y pintura la chapa"},
+    {cercania: 3, distancia: 20, active: true, nombre:"La oficina de globant"},
+    {cercania: 3, distancia: 40, active: true, nombre:"Y la re concha de tu vieja"},
   ];
+
   lat:number = -27.448712;
   lng:number = -58.996193;
   constructor () {
