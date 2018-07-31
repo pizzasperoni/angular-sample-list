bf6cc22171c90fcac972f2910cb2ea75de3d9092
Diego Speroni <diegosperoni1@gmail.com>
2018-07-30 23:52:00 -0300

    Parametros routes

--- src/app/lugares/lugares.component.ts
+++ src/app/lugares/lugares.component.ts
@@ -9,12 +9,12 @@ import { Component } from '@angular/core';
 export class LugaresComponent {
   title = 'Platzi-Squear';
   lugares:any = [
-    {plan:'pagado',cercania: 1, distancia: 1, active: true, nombre:"Floreria la flor"},
-    {plan:'gratuito',cercania: 2, distancia: 5, active: true, nombre:"Jugueteria el juguete"},
-    {plan:'pagado',cercania: 2, distancia: 4, active: true, nombre:"Gomeria la goma"},
-    {plan:'pagado',cercania: 1, distancia: 2, active: true, nombre:"Relojeria el reloj"},
-    {plan:'gratuito',cercania: 3, distancia: 10, active: true, nombre:"Chapa y pintura la chapa"},
-    {plan: 'gratuito',cercania: 3, distancia: 20, active: true, nombre:"La oficina de globant"}
+    {id:1 ,plan:'pagado',cercania: 1, distancia: 1, active: true, nombre:"Floreria la flor"},
+    {id:2 ,plan:'gratuito',cercania: 2, distancia: 5, active: true, nombre:"Jugueteria el juguete"},
+    {id:3 ,plan:'pagado',cercania: 2, distancia: 4, active: true, nombre:"Gomeria la goma"},
+    {id:4 ,plan:'pagado',cercania: 1, distancia: 2, active: true, nombre:"Relojeria el reloj"},
+    {id:5 ,plan:'gratuito',cercania: 3, distancia: 10, active: true, nombre:"Chapa y pintura la chapa"},
+    {id:6 ,plan: 'gratuito',cercania: 3, distancia: 20, active: true, nombre:"La oficina de globant"}
   ];
 
   lat:number = -27.448712;
